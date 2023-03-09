ea68b7308b1699ea8db82cba71aaeb19c915fd47
mariano.palliser <mariano.palliser@gmail.com>
2023-03-09 23:30:40 +0100

    migrate app component to standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
+  template: `
+  App component
+  <router-outlet></router-outlet>
+  `,
 })
 export class AppComponent {
   title = 'pokemon-crud';
