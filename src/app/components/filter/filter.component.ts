cf9e029a8f12cec38abf2062d55784f29c22f3d4
mariano.palliser <mariano.palliser@gmail.com>
2023-03-11 00:10:54 +0100

    real filter implementation

---
+++ src/app/components/filter/filter.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import PokemonService from '@services/pokemon.service';
+import { debounceTime, distinctUntilChanged, Subject, takeUntil } from 'rxjs';
+
+@Component({
+  standalone: true,
+  selector: 'app-filter',
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+  ],
+  templateUrl: './filter.component.html',
+})
+export default class FilterComponent implements OnInit, OnDestroy {
+
+  pokemonFormControl: FormControl = new FormControl('')
+
+  private destroySubscriptions = new Subject()
+
+  constructor(private pokemonService: PokemonService) {}
+
+  ngOnInit(): void {
+    this.pokemonFormControl.valueChanges
+      .pipe(
+        distinctUntilChanged(),
+        debounceTime(500),
+        takeUntil(this.destroySubscriptions)
+      ).subscribe(value => this.pokemonService.fetchPokemon(value))
+  }
+
+  ngOnDestroy(): void {
+    this.destroySubscriptions.next({});
+    this.destroySubscriptions.complete();
+  }
+}
